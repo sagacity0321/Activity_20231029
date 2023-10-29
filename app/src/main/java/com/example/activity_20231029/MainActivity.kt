10af931f6c78932fe46cd6107705fe847f39dbc0
sagacity0321 <citronica0321@gmail.com>
2023-10-29 10:47:58 +0900

    안드로이드 제공 기본앱 - 전화 걸기: DIAL(번호만 입력), CALL(연결까지. 권한 있어야 함) - 문자 전송 - 인터넷 창 - 플레이스토어 링크(업데이트 강제)

--- app/src/main/java/com/example/activity_20231029/MainActivity.kt
+++ app/src/main/java/com/example/activity_20231029/MainActivity.kt
@@ -1,31 +1,42 @@
 package com.example.activity_20231029
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.activity_20231029.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding.btnMoveToSecond.setOnClickListener{
+        binding.btnMoveToSecond.setOnClickListener {
             val myIntent = Intent(this, SecondActivity::class.java)
             startActivity(myIntent)
         }
 
-        binding.btnMoveToThird.setOnClickListener{
+        binding.btnMoveToThird.setOnClickListener {
             val myIntent = Intent(this, ThirdActivity::class.java)
 
+// send message
             val inputMessage = binding.etMessage.text.toString()
             myIntent.putExtra("message", inputMessage)
 
+            // send number
             val inputNumber = binding.etNumber.text.toString().toInt()
             myIntent.putExtra("number", inputNumber) // name 중복 안되면 몇 개든 전송 가능
 
+            // call feat phone number
+            binding.btnDial.setOnClickListener {
+                val phoneNumber = binding.etPhoneNumber.text.toString()
+                val myUri = Uri.parse("tel:${phoneNumber}")
+                val myIntent = Intent(Intent.ACTION_DIAL, myUri)
+                startActivity(myIntent)
+
+            }
 
             startActivity(myIntent)
         }
