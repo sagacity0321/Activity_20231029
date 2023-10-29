210c2673b2b83c2d20e608b950e832e957f6e3df
sagacity0321 <citronica0321@gmail.com>
2023-10-29 09:50:26 +0900

    second activity

--- app/src/main/java/com/example/activity_20231029/MainActivity.kt
+++ app/src/main/java/com/example/activity_20231029/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.example.activity_20231029
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingComponent
+import androidx.databinding.DataBindingUtil
 import com.example.activity_20231029.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btnMoveToSecond.setOnClickListener{
+            val myIntent = Intent(this, SecondActivity::class.java)
+            startActivity(myIntent)
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/activity_20231029/SecondActivity.kt
@@ -0,0 +1,15 @@
+package com.example.activity_20231029
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.activity_20231029.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+
+}
